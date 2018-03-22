75062dc659204b315146e473f15600bd7c94ed86
Unknown <rosys9319@gmail.com>
2018-03-22 16:23:57 -0400

    ajax form submission not working

--- addproducts.php
+++ addproducts.php
@@ -16,6 +16,10 @@ header("location:storelogin.php");
     <meta name="description" content="Bridal Registry Website">
     <meta name="author" content="Rosy Rambani">
     <!-- Bootstrap CSS -->
+    <script
+    src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>Bowring Bridal Registry</title>
@@ -47,36 +51,7 @@ header("location:storelogin.php");
         </div>
       </form>
       
-      <?php
-      include 'connect.php';
-      if (isset($_POST['productadd'])) {
-      $RegCode = $_POST['regcode'];
-      $SkuNumber = $_POST['skunumber'];
-      $Description = $_POST['description'];
-      $Quantity = $_POST['quantity'];
-      $ProductNum =
-      $sql = "INSERT INTO CUSTOMERPRODUCTS (REGISTRYCODE, SKUNUMBER, DESCRIPTION, QUANTITY) VALUES ('$RegCode', '$SkuNumber', '$Description', '$Quantity')";
-      if(!mysqli_query($con, $sql))
-      {
-      echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>Sorry, Data was not saved to the Database!</strong>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-        </button>
-      </div>';
-      }
-      else
-      {
-      echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
-        <strong>Data Successfully Saved to the Database!</strong>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-        </button>
-      </div>';
-      }
-      header('url:addproducts.php');
-      }
-      ?>
+      
       <?php
       //connect to the database
       include('connect.php');
@@ -105,7 +80,6 @@ header("location:storelogin.php");
       $RegistryDb = $row['REGISTRYDATE'];
       $StoreDb = $row['STORE'];
       $EmployeeDb = $row['EMPLOYEENAME'];
-      
       ?>
       <script type="text/javascript">
       function searchResult() {
@@ -123,26 +97,58 @@ header("location:storelogin.php");
       document.getElementById("regcode").value = "<?php echo $SearchCode;?>";
       }
       </script>
-      <script type="text/javascript">
-      var num = 1;
-      
-      function addProductRow() {
-      
-      var e = $('<div class="form-group row"><div class="col-sm-4"><input type="text" name="skunumber" id="skunumber" class="form-control" placeholder="Product SKU" required></div><div class="col-sm-4"><input type="text" name="description" id="description" class="form-control" placeholder="Product Description" required></div><div class="col-sm-4"><input type="text" name="quantity" id="quantity" class="form-control" placeholder="Product Quantity" required></div></div>');
-      $('#productRow').append(e);
-      e.attr('id', 'newProductRow');
-      num += 1;
-      // alert(num);
-      console.log("new row added");
-      }
-      
-      
-      function confirm() {
-      document.getElementById("numOfProducts").value = num; 
-      console.log(num);
-      // alert(num);
-      }
+      <script>
+      $(document).ready(function(){
+        var count = 1;
+        $('#addRow').click(function(){
+        count = count + 1;
+        var e = '<div class="form-group row" id="row'+count+'"><div class="col-sm-3"><input type="text" name="skunumber" id="skunumber" class="form-control item_sku" placeholder="Product SKU" required></div><div class="col-sm-5"><input type="text" name="description" id="description" class="form-control item_description" placeholder="Description" required></div><div class="col-sm-2"><input type="text" name="quantity" id="quantity" class="form-control item_quantity" placeholder="Quantity" required></div><button class="btn btn-danger btn-block col-sm-2 remove" type="button" data-row="row'+count+'" name="remove">Remove</button></div>';
+        $('#productRow').append(e);
+
+        });
+
+      $(document).on('click', '.remove', function(){
+        // alert("remove button clicked");
+        var delete_row = $(this).parent().attr('id');
+        // alert(delete_row);
+        $('#' + delete_row).remove();
+      });
+
+      $('#productadd').click(function(){
+        var item_regcode = [];
+        var item_sku = [];
+        var item_description = [];
+        var item_quantity = [];
+        $('.item_regcode').each(function(){
+          item_regcode.push($(this).text());
+        });
+        $('.item_sku').each(function(){
+          item_sku.push($(this).text());
+        });
+        $('.item_description').each(function(){
+          item_description.push($(this).text());
+        });
+        $('.item_quantity').each(function(){
+          item_quantity.push($(this).text());
+        });
+        $.ajax({
+
+          url:"insert.php",
+          method:"POST",
+          data:{item_regcode:item_regcode, item_sku:item_sku, item_description:item_description, item_quantity:item_quantity},
+          success:function(data){
+            alert(data);
+           
+          }
+        });
+      });
+
+
+
+      });
+
       </script>
+      
       <?php
       echo'
       <form name="updateform" id="updateform" class="card form-registry" method="post" action="">
@@ -209,42 +215,34 @@ header("location:storelogin.php");
           </div>
         </div>
       </form>
-      <form name="updateform" id="updateform" class="card form-addsku" method="post" action="addproducts.php">
+      <form name="updateform" id="updateform" class="card form-addsku" method="post" action="">
         <h3 class="form-title">Add Product Details Here</h3>
         <div class="row">
           <div class="col-sm-4">
             <label for="regcode" class="form-label">Registry Code:</label>
           </div>
           <div class="col-sm-4">
-            <input type="text" name="regcode" id="regcode" class="form-control" placeholder="Registry Code" required readonly>
+            <input type="text" name="regcode" id="regcode" class="form-control item_regcode" placeholder="Registry Code" required readonly>
           </div>
-          <button class="btn btn-success col-sm-4" name="addRow" onClick="addProductRow()" type="submit">Add Row to Enter Product</button>
+          <button class="btn btn-success col-sm-4" name="addRow" id="addRow" type="button">Add Row to Enter Product</button>
         </div>
         <br>
         <div id="productRow">
-          <div class="form-group row">
+          <div class="form-group row" id="firstRow">
             
-            <div class="col-sm-4">
-              <input type="text" name="skunumber" id="skunumber" class="form-control" placeholder="Product SKU" required>
+            <div class="col-sm-3">
+              <input type="text" name="skunumber" id="skunumber" class="form-control item_sku" placeholder="Product SKU" required>
             </div>
-            <div class="col-sm-4">
-              <input type="text" name="description" id="description" class="form-control" placeholder="Product Description" required>
+            <div class="col-sm-5">
+              <input type="text" name="description" id="description" class="form-control item_description" placeholder="Description" required>
             </div>
-            <div class="col-sm-4">
-              <input type="text" name="quantity" id="quantity" class="form-control" placeholder="Product Quantity" required>
+            <div class="col-sm-2">
+              <input type="text" name="quantity" id="quantity" class="form-control item_quantity" placeholder="Quantity" required>
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-sm-4">
-            <label for="numOfProducts" class="form-label">Number of products:</label>
-          </div>
-          <div class="col-sm-4">
-            <input type="text" name="numOfProducts" id="numOfProducts" class="form-control" required readonly>
-          </div>
-          <button class="btn btn-warning btn-block col-sm-4" type="button" id="productConfirm" name="productConfirm" value="confirm" onClick="confirm()"><a>Confirm all Products</a></button>
-        </div>
-        <button class="btn btn-primary btn-block" type="submit" id="productadd" name="productadd" value="update"><a>Add Above Products to Registry</a></button>
+        
+        <button class="btn btn-primary btn-block" type="submit" id="productadd" name="productadd"><a>Add Above Products to Registry</a></button>
       </form>
       <script>
       searchResult();
@@ -262,7 +260,7 @@ header("location:storelogin.php");
       }
       ?>
     </div>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js" integrity="sha384-feJI7QwhOS+hwpX2zkaeJQjeiwlhOP+SdQDqhgvvo1DsjtiSQByFdThsxO669S2D" crossorigin="anonymous"></script>
--- codesnippets.php
+++ codesnippets.php
@@ -206,3 +206,36 @@ echo'<script>
   
   }
 ?>
+
+
+
+<?php
+      include 'connect.php';
+      if (isset($_POST['productadd'])) {
+      $RegCode = $_POST['regcode'];
+      $SkuNumber = $_POST['skunumber'];
+      $Description = $_POST['description'];
+      $Quantity = $_POST['quantity'];
+      $ProductNum =
+      $sql = "INSERT INTO CUSTOMERPRODUCTS (REGISTRYCODE, SKUNUMBER, DESCRIPTION, QUANTITY) VALUES ('$RegCode', '$SkuNumber', '$Description', '$Quantity')";
+      if(!mysqli_query($con, $sql))
+      {
+      echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Sorry, Data was not saved to the Database!</strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+      </div>';
+      }
+      else
+      {
+      echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Data Successfully Saved to the Database!</strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+      </div>';
+      }
+      header('url:addproducts.php');
+      }
+      ?>
